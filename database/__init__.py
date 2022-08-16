efef78a7c4199e1c9197692281d7c33ca8ba8163
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-08-16 13:06:37 +0000

    Changed init of database to be a function (so it will be executed only when needed and not when loaded).

--- database/__init__.py
+++ database/__init__.py
@@ -5,13 +5,18 @@ from sqlalchemy.orm import sessionmaker, Session
 import os
 
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
-
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
+
+
+def SessionLocal(database_url: str=None) -> Session:
+    if database_url is None:
+        database_url = os.environ.get('DATABASE_URL')
+
+    engine = create_engine(database_url)
+    session_maker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    return session_maker()
+
 
 def get_db() -> Session:
     """This is a generator to get a session to the database through SQLAlchemy."""
