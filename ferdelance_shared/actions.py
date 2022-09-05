907d342abf6e37f39b06c5ca32d165414f3064c2
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-09-05 12:22:00 +0000

    Added shared action names to library.

---
+++ ferdelance_shared/actions.py
@@ -0,0 +1,12 @@
+"""Execute a new command."""
+EXEC: str = 'execute'
+"""Train the given model."""
+TRAIN: str = 'train'
+
+"""Token is expied, update with a new one."""
+UPDATE_TOKEN: str = 'update_token'
+"""There is a new client version, update it."""
+UPDATE_CLIENT: str = 'update_client'
+
+"""No action required."""
+DO_NOTHING: str = 'nothing'
