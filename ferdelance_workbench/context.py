0a782725452c92aedc6d8d58ed9f1c13a6b22fd1
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-09-13 11:48:01 +0000

    Extended context with list of clients, details of a client, list of datasource, and detail of datasource.

--- ferdelance_workbench/context.py
+++ ferdelance_workbench/context.py
@@ -1,5 +1,8 @@
+from typing import Any
 import requests
 import logging
+
+from ferdelance_workbench.exceptions import ServerError
 
 LOGGER = logging.getLogger(__name__)
 
@@ -7,15 +10,36 @@ LOGGER = logging.getLogger(__name__)
 class Context:
 
     def __init__(self, server: str) -> None:
-        self.server = server
+        self.server = server.rstrip('/')
 
     def list_clients(self) -> list[str]:
-        res = requests.get(
-            f'{self.server}/manager/client/list'
-        )
+        res = requests.get(f'{self.server}/workbench/client/list')
 
         if res.status_code != 200:
-            LOGGER.error(f'server answered with status code: {res.status_code}')
-            return []
+            raise ServerError(f'server status code: {res.status_code}')
+
+        return res.json()
+
+    def detail_client(self, client_id: str) -> dict[str, Any]:
+        res = requests.get(f'{self.server}/workbench/client/{client_id}')
+
+        if res.status_code != 200:
+            raise ServerError(f'server status code: {res.status_code}')
+
+        return res.json()
+
+    def list_datasources(self) -> list[dict]:
+        res = requests.get(f'{self.server}/workbench/datasource/list/')
+
+        if res.status_code != 200:
+            raise ServerError(f'server status code: {res.status_code}')
+
+        return res.json()
+
+    def detail_datasource(self, datasource_id: int):
+        res = requests.get(f'{self.server}/workbench/datasource/{datasource_id}')
+
+        if res.status_code != 200:
+            raise ServerError(f'server status code: {res.status_code}')
 
         return res.json()
