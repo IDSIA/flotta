32df50480dd457cf66c4246431bf56be34adbd3b
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-09-16 11:51:56 +0000

    Minor.

--- workbench.py
+++ workbench.py
@@ -1,19 +1,24 @@
 # %%
 from ferdelance_workbench.context import Context
+import json
 
 # %%
 
-ctx = Context('http://spearhead.artemis.idsia.ch')
+ctx = Context('http://ferdelance.artemis.idsia.ch')
+
+
+def pretty_print(content: dict):
+    print(json.dumps(content, sort_keys=True, indent=4))
+
 
 # %%
 for c in ctx.list_clients():
-    print(c)
+    pretty_print(ctx.detail_client(c))
 
 # %%
 
 for ds in ctx.list_datasources():
-    print(ds)
-    print(ctx.detail_datasource(ds['datasource_id']))
+    pretty_print(ctx.detail_datasource(ds))
     print()
 
 # %%
