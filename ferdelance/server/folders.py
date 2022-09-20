8359646ca922834a5befe73b16a733f16298765b
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-09-20 12:24:08 +0000

    Added check at api creation for required folders and fixed wrong storage folder in docker-compose.

---
+++ ferdelance/server/folders.py
@@ -0,0 +1,5 @@
+import os
+
+STORAGE_ARTIFACTS: str = str(os.path.join('.', 'storage', 'artifacts'))
+STORAGE_CLIENTS: str = str(os.path.join('.', 'storage', 'clients'))
+STORAGE_MODELS: str = str(os.path.join('.', 'storage', 'models'))
