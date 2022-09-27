1d87565a2fa030dbcead07fdab31b75681e19702
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-09-27 07:18:04 +0000

    Updated workbench example file.

--- workbench.py
+++ workbench.py
@@ -1,6 +1,6 @@
 # %%
 from ferdelance_workbench.context import Context
-from ferdelance_workbench.artifacts import Artifact, Query, Model, Strategy
+from ferdelance_workbench.artifacts import Artifact, ArtifactStatus, Query, QueryFeature, Model, Strategy, DataSource
 
 # %% create the context
 ctx = Context('http://ferdelance.artemis.idsia.ch')
@@ -9,39 +9,47 @@ ctx = Context('http://ferdelance.artemis.idsia.ch')
 for c in ctx.list_clients():
     dc = ctx.detail_client(c)
     print(dc.client_id)
-    print(dc.created_at)
-    print(dc.version)
+    print('- creation time: ', dc.created_at)
+    print('- client version:', dc.version)
 
 # %% ask the context for available metadata
-data_sources_id: list[int] = ctx.list_datasources()
+data_sources_id: list[str] = ctx.list_datasources()
 
 for ds in data_sources_id:
-    dds = ctx.detail_datasource(ds)
-    print(f'{dds.datasource.type:5} {dds.datasource.name}')
+    dds: DataSource = ctx.detail_datasource(ds)
+    print(f'{dds.datasource_id}')
     for df in dds.features:
-        print(f'{df.feature_id:3} {df.dtype:8} {df.name}')
+        print(f'- {df.dtype:8} {df.name:4} {df.v_mean}')
 
 # %% develop a filter query
 ds_id = data_sources_id[0]
 
 dds = ctx.detail_datasource(ds_id)
 
-q = Query()
-q.add_datasource(dds)
+q = Query(
+    datasources_id=dds.datasource_id,
+    features=[QueryFeature(feature_id=f.feature_id, datasource_id=f.datasource_id) for f in dds.features]
+)
 
 # %% develop a model
-m = Model('example_model', None)
+m = Model(name='example_model', model=None)
 
 # %% develop an aggregation strategy
-s = Strategy('nothing')
+s = Strategy(strategy='nothing')
 
 # %% create an artifact and deploy query, model, and strategy to the server
-a: Artifact = ctx.submit(q, m, s)
+a: Artifact = Artifact(
+    queries=[q],
+    model=m,
+    strategy=s,
+)
 
-print(a)
+a, status = ctx.submit(a, True)
+
+print(status)
 
 # %% monitor learning progress
-a = ctx.status(a)
+a: ArtifactStatus = ctx.status(a)
 
 print(a)
 
@@ -51,6 +59,6 @@ art = ctx.get_artifact(a.artifact_id)
 print(art)
 
 # %% download trained model:
-ctx.get_model(a, 'model.bin')
+ctx.get_model_to_disk(a, 'model.bin')
 
 # %%
