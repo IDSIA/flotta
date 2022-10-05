7fc33ec1d055e1d314a7f907ea5d15ecfe896beb
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-10-05 15:47:41 +0000

    Cleaned a little starter script.

--- starter.sh
+++ starter.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
 while [ $# -gt 0 ] ; do
-  case $1 in
-    -t | --target) W="$2" ;;
-  esac
-  shift
+    case $1 in
+        -t | --target) W="$2" ;;
+    esac
+    shift
 done
 
 case $W in
-    worker) cd ferdelance && celery --broker ${CELERY_BROKER_URL} --result-backend ${CELERY_BACKEND_URL} -A ferdelance.worker.celery worker --loglevel=INFO;;
-    flower) cd ferdelance && celery --broker ${CELERY_BROKER_URL} --result-backend ${CELERY_BACKEND_URL} -A ferdelance.worker.celery flower;;
-    server) uvicorn ferdelance.server.api:api --host 0.0.0.0 --port 1456;;
+    worker)
+        cd ferdelance
+        celery \
+            --broker ${CELERY_BROKER_URL} \
+            --result-backend ${CELERY_BACKEND_URL} \
+            --app ferdelance.worker.celery \
+            worker \
+            --loglevel INFO
+        ;;
+    flower)
+        cd ferdelance 
+        celery \
+            --broker ${CELERY_BROKER_URL} \
+            --result-backend ${CELERY_BACKEND_URL} \
+            --app ferdelance.worker.celery \
+            flower
+        ;;
+    server)
+        uvicorn \
+            ferdelance.server.api:api \
+            --host 0.0.0.0 \
+            --port 1456
+        ;;
 esac
