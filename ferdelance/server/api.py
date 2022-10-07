6b2b71251cb1dc88e45a8cb3053ef3bff0581376
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-10-07 10:08:52 +0000

    Added better exception handling for fastapi errors.

--- ferdelance/server/api.py
+++ ferdelance/server/api.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 import uvicorn
 
@@ -40,6 +42,14 @@ async def populate_database() -> None:
 async def root(db: Session = Depends(get_db)):
     """This is the endpoint for the home page."""
     return 'Hi! 😀'
+
+
+@api.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+    LOGGER.error(f"{request}: {exc_str}")
+    content = {'status_code': 10422, 'message': exc_str, 'data': None}
+    return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
 if __name__ == '__main__':
