97d6e4737b8032e8ed0732a0cb927ac4408f3c16
denis.broggini <denis.broggini@supsi.ch>
2022-10-07 13:13:23 +0000

    prepare merge for issue 13 client

--- workbench.py
+++ workbench.py
@@ -3,7 +3,7 @@ from ferdelance_workbench.context import Context
 from ferdelance_workbench.artifacts import Artifact, ArtifactStatus, Dataset, Query, Model, Strategy, DataSource
 
 # %% create the context
-ctx = Context('http://ferdelance.artemis.idsia.ch')
+ctx = Context('http://ferdelance.chronos.idsia.ch')
 
 # %% ask the context for available client
 for c in ctx.list_clients():
@@ -15,17 +15,20 @@ for c in ctx.list_clients():
 # %% ask the context for available metadata
 data_sources_id: list[str] = ctx.list_datasources()
 
-ds: DataSource = None
-
 for ds in data_sources_id:
     dds: DataSource = ctx.detail_datasource(ds)
+    print(dds.name)
+    print(dds.info())
 
-    if dds.name == 'earthquakes':
-        ds = dds
+#     if dds.name == 'earthquakes':
+#         ds = dds
 
-print(ds.info())
+# print(ds.info())
 
 # %% feature analysis
+
+ds = ctx.detail_datasource(data_sources_id[-1])
+
 
 for k in ds.features_dict().keys():
     print(k)
@@ -33,19 +36,21 @@ for k in ds.features_dict().keys():
 # %% develop a filter query
 q: Query = ds.all_features()
 # remove features
-q -= ds['ID']
-q -= ds['Latitude']
-q -= ds['Longitude']
+# q -= ds['ID']
+# q -= ds['Latitude']
+# q -= ds['Longitude']
 
-# add filters
-q = q[ds['Depth'] > 5.0]
-q += ds['Magnitude'] <= 3.0
+
+# # add filters
+# q = q[ds['Depth'] > 5.0]
+# q += ds['Magnitude'] <= 3.0
 
 # %% create dataset
 d = Dataset(
     test_percentage=0.2,
     val_percentage=0.1,
-    label='Magnitude'
+    # label='Magnitude'
+    label='y1'
 )
 d.add_query(q)
 
