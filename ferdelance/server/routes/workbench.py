3b9e8875e08a9aec91d387ba2679e6e458534efb
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-10-11 14:45:05 +0000

    Added token check on workbench routes.

--- ferdelance/server/routes/workbench.py
+++ ferdelance/server/routes/workbench.py
@@ -4,9 +4,11 @@ from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
 
 from ...database import get_db
-from ...database.services import ArtifactService, ClientService, DataSourceService, ModelService
+from ...database.services import ArtifactService, ClientService, DataSourceService
 from ...database.tables import Client, ClientDataSource, Model
 from ..services import JobManagementService
+from ..security import check_token
+
 
 from ferdelance_shared.schemas import ClientDetails, DataSource, Feature, ArtifactStatus, Artifact, WorkbenchJoinData
 
@@ -19,6 +21,17 @@ LOGGER = logging.getLogger(__name__)
 workbench_router = APIRouter()
 
 
+def check_access(db: Session, client_id) -> Client:
+    cs: ClientService = ClientService(db)
+
+    client = cs.get_client_by_id(client_id)
+
+    if client is None or client.type != 'WORKBENCH':
+        raise HTTPException(403)
+
+    return client
+
+
 @workbench_router.get('/workbench/')
 async def wb_home():
     return 'Workbench 🔧'
@@ -26,9 +39,11 @@ async def wb_home():
 
 @workbench_router.get('/workbench/connect', response_model=WorkbenchJoinData)
 async def wb_connect(db: Session = Depends(get_db)):
+    LOGGER.info('new workbench connected')
+
     cs: ClientService = ClientService(db)
 
-    token: str | None = cs.get_token_by_client_type('WORKBENCH')
+    token = cs.get_token_by_client_type('WORKBENCH')
 
     if token is None:
         raise HTTPException(404)
@@ -39,7 +54,11 @@ async def wb_connect(db: Session = Depends(get_db)):
 
 
 @workbench_router.get('/workbench/client/list', response_model=list[str])
-async def wb_get_client_list(db: Session = Depends(get_db)):
+async def wb_get_client_list(db: Session = Depends(get_db), client_id: str = Depends(check_token)):
+    LOGGER.info('a workbench requested a list of clients')
+
+    check_access(db, client_id)
+
     cs: ClientService = ClientService(db)
 
     clients: list[Client] = cs.get_client_list()
@@ -47,13 +66,14 @@ async def wb_get_client_list(db: Session = Depends(get_db)):
     return [m.client_id for m in clients if m.active is True]
 
 
-@workbench_router.get('/workbench/client/{client_id}', response_model=ClientDetails)
-async def wb_get_client_detail(client_id: str, db: Session = Depends(get_db)):
-    cs: ClientService = ClientService(db)
+@workbench_router.get('/workbench/client/{req_client_id}', response_model=ClientDetails)
+async def wb_get_client_detail(req_client_id: str, db: Session = Depends(get_db), client_id: str = Depends(check_token)):
+    LOGGER.info(f'a workbench requested details on client_id={req_client_id}')
 
-    client: Client = cs.get_client_by_id(client_id)
+    client: Client = check_access(db, client_id)
 
-    if client.active is False:
+    if client is None or client.active is False:
+        LOGGER.warn(f'client_id={req_client_id} not found in database or is not active')
         raise HTTPException(404)
 
     return ClientDetails(
@@ -64,7 +84,11 @@ async def wb_get_client_detail(client_id: str, db: Session = Depends(get_db)):
 
 
 @workbench_router.get('/workbench/datasource/list', response_model=list[str])
-async def wb_get_datasource_list(db: Session = Depends(get_db)):
+async def wb_get_datasource_list(db: Session = Depends(get_db), client_id: str = Depends(check_token)):
+    LOGGER.info('a workbench requested a list of available data source')
+
+    check_access(db, client_id)
+
     dss: DataSourceService = DataSourceService(db)
 
     ds_db: list[ClientDataSource] = dss.get_datasource_list()
@@ -75,12 +99,17 @@ async def wb_get_datasource_list(db: Session = Depends(get_db)):
 
 
 @workbench_router.get('/workbench/datasource/{ds_id}', response_model=DataSource)
-async def wb_get_client_datasource(ds_id: str, db: Session = Depends(get_db)):
+async def wb_get_client_datasource(ds_id: str, db: Session = Depends(get_db), client_id: str = Depends(check_token)):
+    LOGGER.info(f'a workbench requested details on datasource_id={ds_id}')
+
+    check_access(db, client_id)
+
     dss: DataSourceService = DataSourceService(db)
 
     ds_db: ClientDataSource = dss.get_datasource_by_id(ds_id)
 
     if ds_db is None or ds_db.removed is True:
+        LOGGER.warn(f'datasource_id={ds_id} not found in database or has been removed')
         raise HTTPException(404)
 
     f_db = dss.get_features_by_datasource(ds_db)
@@ -94,10 +123,16 @@ async def wb_get_client_datasource(ds_id: str, db: Session = Depends(get_db)):
 
 
 @workbench_router.post('/workbench/artifact/submit', response_model=ArtifactStatus)
-def wb_post_artifact_submit(artifact: Artifact, db: Session = Depends(get_db)):
+def wb_post_artifact_submit(artifact: Artifact, db: Session = Depends(get_db), client_id: str = Depends(check_token)):
+    check_access(db, client_id)
+
     try:
         jms: JobManagementService = JobManagementService(db)
-        return jms.submit_artifact(artifact)
+        status = jms.submit_artifact(artifact)
+
+        LOGGER.info(f'a workbench submitted a new with assigned artifact_id={status.artifact_id}')
+
+        return status
 
     except ValueError as e:
         LOGGER.error('Artifact already exists')
@@ -106,7 +141,11 @@ def wb_post_artifact_submit(artifact: Artifact, db: Session = Depends(get_db)):
 
 
 @workbench_router.get('/workbench/artifact/status/{artifact_id}', response_model=ArtifactStatus)
-async def wb_get_artifact_status(artifact_id: str, db: Session = Depends(get_db)):
+async def wb_get_artifact_status(artifact_id: str, db: Session = Depends(get_db), client_id: str = Depends(check_token)):
+    LOGGER.info(f'a workbench requested status of artifact_id={artifact_id}')
+
+    check_access(db, client_id)
+
     ars: ArtifactService = ArtifactService(db)
 
     artifact_db = ars.get_artifact(artifact_id)
@@ -114,6 +153,7 @@ async def wb_get_artifact_status(artifact_id: str, db: Session = Depends(get_db)
     # TODO: get status from celery
 
     if artifact_db is None:
+        LOGGER.warn(f'artifact_id={artifact_id} not found in database')
         raise HTTPException(404)
 
     return ArtifactStatus(
@@ -123,7 +163,11 @@ async def wb_get_artifact_status(artifact_id: str, db: Session = Depends(get_db)
 
 
 @workbench_router.get('/workbench/artifact/{artifact_id}', response_model=Artifact)
-async def wb_get_artifact(artifact_id: str, db: Session = Depends(get_db)):
+async def wb_get_artifact(artifact_id: str, db: Session = Depends(get_db), client_id: str = Depends(check_token)):
+    LOGGER.info(f'a workbench requested details on artifact_id={artifact_id}')
+
+    check_access(db, client_id)
+
     try:
         jms: JobManagementService = JobManagementService(db)
         return jms.get_artifact(artifact_id)
@@ -134,7 +178,11 @@ async def wb_get_artifact(artifact_id: str, db: Session = Depends(get_db)):
 
 
 @workbench_router.get('/workbench/model/{artifact_id}', response_class=FileResponse)
-async def wb_get_model(artifact_id: str, db: Session = Depends(get_db)):
+async def wb_get_model(artifact_id: str, db: Session = Depends(get_db), client_id: str = Depends(check_token)):
+    LOGGER.info(f'a workbench requested aggregate model for artifact_id={artifact_id}')
+
+    check_access(db, client_id)
+
     ars: ArtifactService = ArtifactService(db)
 
     model_dbs: list[Model] = ars.get_models_by_artifact_id(artifact_id)
@@ -144,7 +192,10 @@ async def wb_get_model(artifact_id: str, db: Session = Depends(get_db)):
 
     model_db = [m for m in model_dbs if m.aggregated][0]
 
-    if not os.path.exists(model_db.path):
+    model_path = model_db.path
+
+    if not os.path.exists(model_path):
+        LOGGER.warn(f'model_id={model_db.model_id} not found at path={model_path}')
         raise HTTPException(404)
 
-    return FileResponse(model_db.path)
+    return FileResponse(model_path)
