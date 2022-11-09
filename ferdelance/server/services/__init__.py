bd774992ddc8ab71682bc4b360e8efcaeedf9186
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-11-09 10:32:19 +0000

    Added token service.

--- ferdelance/server/services/__init__.py
+++ ferdelance/server/services/__init__.py
@@ -1,9 +1,11 @@
 __all__ = [
     'ActionService',
     'SecurityService',
+    'TokenService',
     'JobManagementService',
 ]
 
 from .security import SecurityService
+from .tokens import TokenService
 from .actions import ActionService
 from .jobs import JobManagementService
