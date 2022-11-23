a3c1e63acbe4165d2a3e2a29e7b57cb19669adbd
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-11-23 08:23:17 +0000

    Removed code moved to shared lib.

--- ferdelance_workbench/artifacts/artifact.py
+++
@@ -1,7 +0,0 @@
-from ferdelance_shared.schemas import Artifact as BaseArtifact
-from .dataset import Dataset
-
-
-class Artifact(BaseArtifact):
-    dataset: Dataset
-    artifact_id: str | None = None
