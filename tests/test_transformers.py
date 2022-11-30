2f74a5a13899b0642b55c2bbb9a8d35f6afba729
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-11-30 08:25:37 +0000

    Added stub for transformer.

---
+++ tests/test_transformers.py
@@ -0,0 +1,9 @@
+from ferdelance_shared.transformers.scaling import FederatedMinMaxScaler
+
+
+class TestTransformers:
+
+    def test_scaling(self):
+        print('this will give an error!')
+
+        fmms = FederatedMinMaxScaler()
