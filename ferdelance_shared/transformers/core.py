de44705ec0cf50b3f17fcf9380f21f27acde10a4
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-12-01 14:08:39 +0000

    Moved some init stuff to Transfomer (renamed from GenericTransformer).

--- ferdelance_shared/transformers/core.py
+++ ferdelance_shared/transformers/core.py
@@ -1,11 +1,35 @@
+from typing import Any
+
 from ..artifacts.queries import QueryTransformer, QueryFeature
 
+import pandas as pd
+import pickle
 
-class GenericTransformer:
-    name: str
+
+class Transformer:
+
+    def __init__(self, name: str, features_in: QueryFeature | list[QueryFeature] | str | list[str], features_out: QueryFeature | list[QueryFeature] | str | list[str],) -> None:
+        self.name: str = name
+        self.features_in: list[str] = convert_features_to_list(features_in)
+        self.features_out: list[str] = convert_features_to_list(features_out)
+
+        if len(self.features_in) != len(self.features_out):
+            raise ValueError('Input and output features are not of the same length')
+
+    def params(self) -> dict[str, Any]:
+        return dict()
+
+    def dict(self) -> dict[str, Any]:
+        return {
+            'features_in': self.features_in,
+            'features_out': self.features_out,
+            'name': self.name,
+            'parameters': self.params(),
+        }
 
     def save(self, path: str) -> None:
-        raise NotImplementedError()
+        with open(path, 'wb') as f:
+            pickle.dump(self.dict(), f)
 
     def load(self, path: str) -> None:
         raise NotImplementedError()
@@ -13,14 +37,14 @@ class GenericTransformer:
     def aggregate(self) -> None:
         raise NotImplementedError()
 
-    def fit(self) -> None:
+    def fit(self, df: pd.DataFrame) -> None:
         raise NotImplementedError()
 
-    def transform(self) -> None:
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
         raise NotImplementedError()
 
     def build(self) -> QueryTransformer:
-        raise NotImplementedError()
+        return QueryTransformer(**self.dict())
 
 
 def convert_features_to_list(features: QueryFeature | list[QueryFeature] | str | list[str]) -> list[str]:
--- ferdelance_shared/transformers/scaling.py
+++ ferdelance_shared/transformers/scaling.py
@@ -1,10 +1,8 @@
-from .core import (
-    GenericTransformer,
-    convert_features_to_list
-)
+from typing import Any
+
+from .core import Transformer
 from ..artifacts import (
     QueryFeature,
-    QueryTransformer,
 )
 
 from sklearn.preprocessing import MinMaxScaler
@@ -13,44 +11,32 @@ import pandas as pd
 import pickle
 
 
-class FederatedMinMaxScaler(GenericTransformer):
+class FederatedMinMaxScaler(Transformer):
 
-    def __init__(self,
-                 features_in: QueryFeature | list[QueryFeature] | str | list[str],
-                 features_out: QueryFeature | list[QueryFeature] | str | list[str],
-                 feature_range: tuple = (0, 1)
-                 ) -> None:
-        super().__init__()
+    def __init__(self, features_in: QueryFeature | list[QueryFeature] | str | list[str], features_out: QueryFeature | list[QueryFeature] | str | list[str], feature_range: tuple = (0, 1)) -> None:
+        super().__init__(FederatedMinMaxScaler.__name__, features_in, features_out)
 
         self.scaler: MinMaxScaler = MinMaxScaler(feature_range=feature_range)
-
-        self.features_in: list[str] = convert_features_to_list(features_in)
-        self.features_out: list[str] = convert_features_to_list(features_out)
         self.feature_range: tuple[float, float] = feature_range
 
-        if len(self.features_in) != len(self.features_out):
-            raise ValueError('Input and output features are not of the same length')
+    def params(self) -> dict[str, Any]:
+        return super().params() | {
+            'feature_range': self.feature_range,
+        }
 
-    def save(self, path: str) -> None:
-        with open(path, 'wb') as f:
-            pickle.dump({
-                'features_in': self.features_in,
-                'features_out': self.features_out,
-                'feature_range': self.feature_range,
-                'scaler': self.scaler,
-            }, f)
+    def dict(self) -> dict[str, Any]:
+        return super().dict() | {
+            'scaler': self.scaler,
+        }
 
     def load(self, path: str) -> None:
         with open(path, 'rb') as f:
             data = pickle.load(f)
+            self.name = data['name']
             self.features_in = data['features_in']
             self.features_out = data['features_out']
             self.feature_range = data['feature_range']
             self.scaler = data['scaler']
-
-    def aggregate(self) -> None:
-        # TODO
-        return super().aggregate()
 
     def fit(self, df: pd.DataFrame) -> None:
         self.scaler.fit(df[self.features_in])
@@ -59,12 +45,6 @@ class FederatedMinMaxScaler(GenericTransformer):
         df[self.features_out] = self.scaler.transform(df[self.features_in])
         return df
 
-    def build(self) -> QueryTransformer:
-        return QueryTransformer(
-            features_in=self.features_in,
-            features_out=self.features_out,
-            name=FederatedMinMaxScaler.__name__,
-            parameters={
-                'feature_range': self.feature_range
-            },
-        )
+    def aggregate(self) -> None:
+        # TODO
+        return super().aggregate()
