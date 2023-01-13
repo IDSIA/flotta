ecadd8e71a756b12e5306c07e234762bba676cfe
denis.broggini <denis.broggini@supsi.ch>
2023-01-13 16:32:52 +0000

    db test begin

--- ferdelance/standalone/__main__.py
+++ ferdelance/standalone/__main__.py
@@ -1,34 +1,29 @@
-from ferdelance.client.arguments import setup_arguments
-from ferdelance.config import conf
-from ferdelance.standalone.processes import (
-    LocalWorker,
-    LocalClient,
-    LocalServer,
-)
-
+import logging
 from multiprocessing import Queue
 from multiprocessing.managers import BaseManager
 
-import logging
+from ferdelance.client.arguments import setup_arguments
+from ferdelance.config import conf
+from ferdelance.standalone.processes import LocalClient, LocalServer, LocalWorker
 
 LOGGER = logging.getLogger(__name__)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     client_args = setup_arguments()
 
-    LOGGER.info('standalone application starting')
+    LOGGER.info("standalone application starting")
 
     conf.STANDALONE = True
-    conf.SERVER_MAIN_PASSWORD = '7386ee647d14852db417a0eacb46c0499909aee90671395cb5e7a2f861f68ca1'  # this is a dummy key
-    conf.DB_DIALECT = 'sqlite'
-    conf.DB_MEMORY = True
-    conf.SERVER_INTERFACE = '0.0.0.0'
+    conf.SERVER_MAIN_PASSWORD = "7386ee647d14852db417a0eacb46c0499909aee90671395cb5e7a2f861f68ca1"  # this is a dummy key
+    conf.DB_DIALECT = "sqlite"
+    # conf.DB_MEMORY = True
+    conf.SERVER_INTERFACE = "0.0.0.0"
 
     aggregation_queue = Queue()
 
-    manager = BaseManager(address=('', 14560))
-    manager.register('get_queue', callable=lambda: aggregation_queue)
+    manager = BaseManager(address=("", 14560))
+    manager.register("get_queue", callable=lambda: aggregation_queue)
     manager.start()
 
     server_process = LocalServer()
@@ -45,4 +40,4 @@ if __name__ == '__main__':
 
     manager.shutdown()
 
-    LOGGER.info('standalone application terminated')
+    LOGGER.info("standalone application terminated")
--- tests/server/cli/test_base.py
+++ tests/server/cli/test_base.py
@@ -4,40 +4,46 @@ from ferdelance.server.cli.artifacts import artifacts_cli_suite
 from ferdelance.server.cli.base import CLICommandSuite, CLIParser
 
 
-class TestCLIBase:
+@pytest.fixture
+def parser() -> CLIParser:
+    """An empty CLIParser"""
+    return CLIParser(
+        prog="Ferdelance Admin CLI",
+        description="Command Line Interface to administrate the server",
+    )
 
-    def setup_class(self):
-        """Create empty CLI"""
-        self.parser: CLIParser = CLIParser(
-            prog="Ferdelance Admin CLI",
-            description="Command Line Interface to administrate the server",
-        )
-        self.suite: CLICommandSuite = artifacts_cli_suite
 
-        
+@pytest.fixture
+def suite() -> CLICommandSuite:
+    """An arbitrary command suite"""
+    return artifacts_cli_suite
 
-    @pytest.mark.asyncio
-    async def test_empty_cli(self):
-        with pytest.raises(ValueError) as e:
-            self.parser.parse_args(["clients", "ls"])
-        assert "No command suite found for entity" in str(e)
-        assert self.parser.selected_suite is None
 
-    @pytest.mark.asyncio
-    async def test_incompatible_command_cli(self):
-        self.parser.add_command_suite(self.suite)
-        with pytest.raises(ValueError) as e:
-            self.parser.parse_args(["artifacts", "non_existent_command"])
-        assert "does not support command" in str(e)
-        assert self.parser.selected_command is None
+@pytest.mark.asyncio
+async def test_empty_cli(parser, suite):
+    with pytest.raises(ValueError) as e:
+        parser.parse_args(["clients", "ls"])
+    assert "No command suite found for entity" in str(e)
+    assert parser.selected_suite is None
+    parser.add_command_suite(suite)
+    with pytest.raises(ValueError) as e:
+        parser.parse_args(["clients", "ls"])
+    assert "No command suite found for entity" in str(e)
+    assert parser.selected_suite is None
 
-    @pytest.mark.asyncio
-    async def test_suite_command_found_cli(self):
-        self.parser.add_command_suite(self.suite)
-        self.parser.parse_args(["artifacts", "ls"])
-        assert self.parser.selected_suite is not None
-        assert self.parser.selected_command is not None
 
-    
-    
-            
+@pytest.mark.asyncio
+async def test_incompatible_command_cli(parser, suite):
+    parser.add_command_suite(suite)
+    with pytest.raises(ValueError) as e:
+        parser.parse_args(["artifacts", "non_existent_command"])
+    assert "does not support command" in str(e)
+    assert parser.selected_command is None
+
+
+@pytest.mark.asyncio
+async def test_suite_command_found_cli(parser, suite):
+    parser.add_command_suite(suite)
+    parser.parse_args(["artifacts", "ls"])
+    assert parser.selected_suite is not None
+    assert parser.selected_command is not None
