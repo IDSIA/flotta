dc4b1e554d314bf2e7a578be50f1014fda112d70
denis.broggini <denis.broggini@supsi.ch>
2023-01-24 10:51:58 +0000

    added projects cli suite

--- ferdelance/cli/suites/__init__.py
+++ ferdelance/cli/suites/__init__.py
@@ -1,9 +1,4 @@
-__all__ = [
-    "clients_cli_suite",
-    "artifacts_cli_suite",
-    "jobs_cli_suite",
-    "models_cli_suite",
-]
+__all__ = ["clients_cli_suite", "artifacts_cli_suite", "jobs_cli_suite", "models_cli_suite", "projects_cli_suite"]
 
 
 from .artifacts import artifacts_cli_suite
@@ -11,3 +6,4 @@ from .clients import clients_cli_suite
 from .datasources import datasources_cli_suite
 from .jobs import jobs_cli_suite
 from .models import models_cli_suite
+from .projects import projects_cli_suite
