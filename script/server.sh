c18d45377fcacc4fc12ed23c5bc5f2c4a44e6da7
Claudio Bonesana <claudio.bonesana@supsi.ch>
2023-08-04 14:50:29 +0000

    Updated scripts for server.

--- script/server.sh
+++ script/server.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
 
-while [ $# -gt 0 ] ; do
-    case $1 in
-        -t | --target) W="$2" ;;
-    esac
-    shift
-done
-
-case $W in
-    worker)
-        celery \
-            --broker ${CELERY_BROKER_URL} \
-            --result-backend ${CELERY_BACKEND_URL} \
-            --app ferdelance.worker.celery \
-            worker \
-            --loglevel INFO
-        ;;
-    server)
-        uvicorn \
-            ferdelance.server.api:api \
-            --host 0.0.0.0 \
-            --port 1456 \
-            --log-level info
-        ;;
-esac
+python -m ferdelance.server
