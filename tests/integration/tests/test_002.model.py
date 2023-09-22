f9895fee995465db4f43fd9f387b3fc21f97c6e5
Claudio Bonesana <claudio.bonesana@supsi.ch>
2023-09-22 14:45:49 +0000

    Updated integration tests to latest version.

--- tests/integration/tests/test_002.model.py
+++ tests/integration/tests/test_002.model.py
@@ -6,6 +6,7 @@ from ferdelance.schemas.models import (
 )
 from ferdelance.schemas.plans import TrainTestSplit
 from ferdelance.workbench.context import Context
+from ferdelance.logging import get_logger
 
 from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score, f1_score
 
@@ -14,6 +15,8 @@ import pandas as pd
 import time
 import os
 import sys
+
+LOGGER = get_logger(__name__)
 
 
 def evaluate(model: FederatedRandomForestClassifier, x, y):
@@ -26,11 +29,11 @@ def evaluate(model: FederatedRandomForestClassifier, x, y):
 
     cm = confusion_matrix(y, pred)
 
-    print("Accuracy:", ac)
-    print("F1:      ", f1)
-    print("ROC AUC: ", ra)
+    LOGGER.info(f"Accuracy: {ac}")
+    LOGGER.info(f"F1:       {f1}")
+    LOGGER.info(f"ROC AUC:  {ra}")
 
-    print(cm)
+    LOGGER.info(cm)
 
 
 if __name__ == "__main__":
@@ -38,11 +41,11 @@ if __name__ == "__main__":
     server: str = os.environ.get("SERVER", "")
 
     if not project_id:
-        print("Project id not found")
+        LOGGER.info("Project id not found")
         sys.exit(-1)
 
     if not server:
-        print("Server host not found")
+        LOGGER.info("Server host not found")
         sys.exit(-1)
 
     ctx = Context(server)
@@ -66,7 +69,7 @@ if __name__ == "__main__":
 
     a: Artifact = ctx.submit(project, q)
 
-    print("Artifact id:", a.id)
+    LOGGER.info(f"Artifact id: {a.id}")
 
     last_state = ""
 
@@ -75,45 +78,45 @@ if __name__ == "__main__":
 
     while (status := ctx.status(a)).status != "COMPLETED":
         if status.status == last_state:
-            print(".", end="", flush=True)
+            LOGGER.info(".")
         else:
             last_state = status.status
             start_time = time.time()
-            print(last_state, ".", end="", flush=True)
+            LOGGER.info(last_state)
 
         time.sleep(wait_time)
 
         if time.time() - start_time > max_wait:
-            print("reached max wait time")
+            LOGGER.info("reached max wait time")
             sys.exit(-1)
 
-    print("done!")
+    LOGGER.info("done!")
 
     cls_agg = ctx.get_result(a)
 
-    print("aggregated model fetched:", cls_agg)
+    LOGGER.info(f"aggregated model fetched: {cls_agg}")
 
-    cls_pa1 = ctx.get_partial_result(a, client_id_1)
+    cls_pa1 = ctx.get_partial_result(a, client_id_1, 0)
 
-    print("partial model 1 fetched: ", cls_pa1)
+    LOGGER.info(f"partial model 1 fetched:  {cls_pa1}")
 
-    cls_pa2 = ctx.get_partial_result(a, client_id_2)
+    cls_pa2 = ctx.get_partial_result(a, client_id_2, 0)
 
-    print("partial model 2 fetched: ", cls_pa2)
+    LOGGER.info(f"partial model 2 fetched:  {cls_pa2}")
 
     df = pd.read_csv("/data/california_housing.validation.csv")
 
     X = df.drop("MedHouseValDiscrete", axis=1).values
     Y = df["MedHouseValDiscrete"]
 
-    print("Partial Model 1")
+    LOGGER.info("Partial Model 1")
     evaluate(cls_pa1, X, Y)
-    print()
+    LOGGER.info("")
 
-    print("Partial Model 2")
+    LOGGER.info("Partial Model 2")
     evaluate(cls_pa2, X, Y)
-    print()
+    LOGGER.info("")
 
-    print("Aggregated model")
+    LOGGER.info("Aggregated model")
     evaluate(cls_agg, X, Y)
-    print()
+    LOGGER.info("")
