468aa1770dacb16a09786a281fafad2641a79d73
Claudio Bonesana <claudio.bonesana@supsi.ch>
2023-11-02 15:49:07 +0000

    Updated cli.

--- ferdelance/cli/fdl_suites/resources/functions.py
+++ ferdelance/cli/fdl_suites/resources/functions.py
@@ -1,14 +1,14 @@
 """Implementation of the CLI features regarding models"""
 
+from ferdelance.cli.visualization import show_many, show_one
+from ferdelance.database import DataBase
+from ferdelance.database.repositories import ResourceRepository
+from ferdelance.schemas.database import Resource
+
 from sqlalchemy.exc import NoResultFound
 
-from ferdelance.database import DataBase
-from ferdelance.schemas.database import Resource
-from ferdelance.database.repositories import ResourceRepository
-from ferdelance.cli.visualization import show_many, show_one
 
-
-async def list_models(artifact_id: str | None = None) -> list[Resource]:
+async def list_resource(artifact_id: str | None = None) -> list[Resource]:
     """Print model list, with or without filters on ARTIFACT_ID, MODEL_ID"""
     # TODO depending on 1:1, 1:n relations with artifacts arguments change or disappear
 
@@ -17,20 +17,20 @@ async def list_models(artifact_id: str | None = None) -> list[Resource]:
         resource_repository = ResourceRepository(session)
 
         if artifact_id is not None:
-            models: list[Resource] = await resource_repository.list_models_by_artifact_id(artifact_id)
+            resources: list[Resource] = await resource_repository.list_resources_by_artifact_id(artifact_id)
         else:
-            models: list[Resource] = await resource_repository.list_models()
+            resources: list[Resource] = await resource_repository.list_resources()
 
-        show_many(models)
+        show_many(resources)
 
-        return models
+        return resources
 
 
-async def describe_model(task_id: str | None = None) -> Resource | None:
+async def describe_resource(resource_id: str | None = None) -> Resource | None:
     """Describe single model by printing its db record.
 
     Args:
-        model_id (str): Unique ID of the model
+        resource_id (str): Unique ID of the resource
 
     Raises:
         ValueError: if no model id is provided
@@ -39,7 +39,7 @@ async def describe_model(task_id: str | None = None) -> Resource | None:
         ServerTask: the Task object produced by a client
     """
 
-    if task_id is None:
+    if resource_id is None:
         raise ValueError("Provide a Task ID")
 
     db = DataBase()
@@ -47,10 +47,10 @@ async def describe_model(task_id: str | None = None) -> Resource | None:
         task_repository = ResourceRepository(session)
 
         try:
-            model: Resource = await task_repository.get_model_by_id(task_id)
-            show_one(model)
+            resource: Resource = await task_repository.get_by_id(resource_id)
+            show_one(resource)
 
-            return model
+            return resource
 
-        except NoResultFound as e:
-            print(f"No model found with id {task_id}")
+        except NoResultFound as _:
+            print(f"No model found with id {resource_id}")
--- ferdelance/cli/fdl_suites/resources/suite.py
+++ ferdelance/cli/fdl_suites/resources/suite.py
@@ -3,7 +3,7 @@
 from ferdelance.cli.base import CLICommand, CLICommandSuite
 from ferdelance.cli.fdl_suites.args import FDLCLIArgs
 from ferdelance.cli.fdl_suites.commands import FDLCommands
-from .functions import list_models
+from .functions import list_resource
 
 #
 #   COMMANDS
@@ -12,7 +12,7 @@ from .functions import list_models
 list_command: CLICommand = CLICommand(
     command=FDLCommands.list,
     arguments=[FDLCLIArgs.ARTIFACT_ID, FDLCLIArgs.MODEL_ID],
-    function=list_models,
+    function=list_resource,
 )
 
 #
